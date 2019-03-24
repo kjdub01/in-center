21ad591232ad440d914dc35d9ec65ba286533076
kjdub01 <kjewettwilliams@gmail.com>
2019-03-23 20:04:58 -0700

    update signup routes and create signup form

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get '/signup', to: 'users#new', as: :new_user
+  post '/signup', to: 'users#create', as: :user
   resources :users, only: [:edit, :update, :destroy]
   resources :teams
   resources :games
