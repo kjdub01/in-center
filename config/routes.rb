8c2412331649560c390b7a39f8349bd32cf8eae7
kjdub01 <kjewettwilliams@gmail.com>
2019-03-23 19:03:37 -0700

    update routes and add new and create methods to user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users, only: [:edit, :update, :destroy]
+  resources :teams
+  resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
