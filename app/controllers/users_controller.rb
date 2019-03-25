95a2702645a7646a0e3c369f579c63ff40dc4c27
kjdub01 <kjewettwilliams@gmail.com>
2019-03-24 19:19:56 -0700

    add rest of logic to create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
 
+      redirect_to user_path(@user)
     else
       render "new"
-    end 
+    end
   end
 
   private
