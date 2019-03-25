3000ed13cc7af847ebf38db7c3ccf3b3c1cc3c4f
kjdub01 <kjewettwilliams@gmail.com>
2019-03-24 19:13:38 -0700

    add some logic create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,28 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      render "new"
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :phone,
+      :address1,
+      :address2,
+      :city,
+      :state,
+      :zipcode,
+      :email,
+      :password,
+      :admin
+     )
   end
 end
