efb689554c1d643f55abc0df85175959bb87fabb
kjdub01 <kjewettwilliams@gmail.com>
2019-03-25 18:58:09 -0700

    add methods to ensure users are login to view certain content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+  before_action :required_logged_in, except: [:new, :create, :home]
+
+  def logged_in?
+    !!curent_user
+  end
+
+  private
+
+  def required_logged_in
+    redirect_to root_path unless logged_in?
+  end
+
+  def curent_user
+    @curent_user ||= user.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :curent_user
 end
