79541f8f96c52d534a6e27e48464f06dba96b7e8
kjdub01 <kjewettwilliams@gmail.com>
2019-03-26 21:59:36 -0700

    refactor create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user =User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth[ 'info' ][ 'name' ]
-      u.email = auth[ 'info' ][ 'email' ]
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by_omniauth(auth_hash)
+
+      session[:user_id] = @user.id
+
+      redirect_to user_path(@user)
+    else
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:passowrd])
+        session[:user_id] = @user.id
+
+        redirect_to user_path(@user)
+      else
+        render "sessions/new"
+      end
     end
-
-    session[:user_id] = @user.id
-
-    render 'users/show'
   end
 
   def destroy
