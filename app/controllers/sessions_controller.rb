bdf55a8668a6c7a9fac6e2b52207fb66c457f4fa
kjdub01 <kjewettwilliams@gmail.com>
2019-03-26 18:00:45 -0700

    get create and destroy methods working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user =User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth[ 'info' ][ 'name' ]
+      u.email = auth[ 'info' ][ 'email' ]
+    end
+
+    session[:user_id] = @user.id
+
+    render 'users/show'
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+  private
+
+  def auth
+      request.env[ 'omniauth.auth']
   end
 end
