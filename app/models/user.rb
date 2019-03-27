f3e20a5d91de21b2916f7be8230daafb384f5651
kjdub01 <kjewettwilliams@gmail.com>
2019-03-26 21:58:19 -0700

    add find_or_create_by_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   VAILD_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,length: { maximum: 255}, format: {with: VAILD_EMAIL_FORMAT}, uniqueness: { case_sensitive: false }
 
+
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(email: auth_hash["info"]["email"]).first_or_create do |u|
+      u.password = SecureRandom.hex
+      u.name = auth_hash["info"]["name"]
+    end
+  end
 end
