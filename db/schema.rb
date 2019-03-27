9dfdc6fed6743e5b49a0df48e82c7e6b783d36d1
kjdub01 <kjewettwilliams@gmail.com>
2019-03-26 17:56:19 -0700

    add uid to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_024944) do
+ActiveRecord::Schema.define(version: 2019_03_26_023959) do
 
   create_table "games", force: :cascade do |t|
     t.integer "user_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_024944) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
 end
