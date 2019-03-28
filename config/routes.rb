57a336d073ca16d2079879754f8c756c90b216b5
kjdub01 <kjewettwilliams@gmail.com>
2019-03-27 20:56:44 -0700

    nest game resource show and index under users

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/auth/facebook/callback' => 'sessions#create'
-  resources :users
-  resources :teams
+  resources :users do
+    resources :games, only: [:show, :index]
+  end
   resources :games
+  resources :teams
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
