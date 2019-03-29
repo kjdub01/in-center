b73076156a8cae7b9ca0d10af9b8885b4cdc1cc7
kjdub01 <kjewettwilliams@gmail.com>
2019-03-29 11:45:36 -0700

    fix regex for phone number

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,13 @@
 class Team < ApplicationRecord
   has_many :teamgames
-  has_many :games, through: :gamesteams  
+  has_many :games, through: :gamesteams
+
+  validates :team_name, presence: true
+  validates :contact_name, presence: true
+  VALID_PHONE_FORMAT = /^[2-9]\d{2}-\d{3}-\d{4}$/
+  validates :contact_phone, presence: true, format: {with: VAILD_PHONE_FORMAT}
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true length: {minimum: 5, maximum: 5}
 end
