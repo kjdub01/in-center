ab79855adecff0d5da90ae1537facd141d3c4560
kjdub01 <kjewettwilliams@gmail.com>
2019-03-28 20:49:32 -0700

    add full CRUD methods and logic for create

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,6 @@
 class TeamsController < ApplicationController
-
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+  
   def index
     if params[:game_id]
       @game = Game.find_by(id: params[:game_id])
@@ -24,4 +25,45 @@ class TeamsController < ApplicationController
       @games = Game.find(params[:id])
     end
   end
+
+  def new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    respond_to do |format|
+      if @team.save
+        format.html {redirect_to @team, notice: "The team was successfully created"}
+      else
+        format.html {render :new}
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(
+      :team_name,
+      :team_contact,
+      :address1,
+      :address2,
+      :city,
+      :state,
+      :zipcode)
+  end
+
 end
