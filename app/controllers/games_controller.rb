5e7be402c2e1f2ed15da24a796a84affbc34e65b
kjdub01 <kjewettwilliams@gmail.com>
2019-03-31 13:29:32 -0700

    add logic for show and create methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,29 @@ class GamesController < ApplicationController
   end
 
   def show
+    @game = Game.find(params[:id])
+  end
 
+  def new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:user_id, :starts_at)
   end
 
 
