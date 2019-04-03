0932b4b90cf639483ef3816e9ba320aa3b66b1d6
kjdub01 <kjewettwilliams@gmail.com>
2019-04-03 09:59:33 -0700

    add line to  improve readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_secure_password
   has_many :games
+
   validates :name, presence: true, length: { maximum: 70}
   VAILD_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,length: { maximum: 255}, format: {with: VAILD_EMAIL_FORMAT}, uniqueness: { case_sensitive: false }
