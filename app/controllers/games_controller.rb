0f9f91bf6693cbcc2c24e4de1404d892a5f9eba0
kjdub01 <kjewettwilliams@gmail.com>
2019-04-03 23:54:08 -0700

    correct new and game_params for nested form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,14 +5,18 @@ class GamesController < ApplicationController
   end
 
   def show
+    @home_team = @game.teams[0]
+    @away_team = @game.teams[1]
   end
 
   def new
     @game = Game.new
+    2.times { @game.games_team.build }
   end
 
   def create
     @game = Game.new(game_params)
+
     if @game.save
       redirect_to game_path(@game)
     else
@@ -27,7 +31,7 @@ class GamesController < ApplicationController
   end
 
   def game_params
-    params.require(:game).permit(:user_id, :starts_at)
+    params.require(:game).permit(:user_id, :starts_at, games_team_attributes:[:id, :game_id, :team_id, :home_team])
   end
 
 
