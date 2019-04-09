493dcaa97e307234cfe89fda25c6d8703a4a6437
kjdub01 <kjewettwilliams@gmail.com>
2019-04-09 15:46:03 -0700

    add set_timezone to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :current_user
   before_action :require_login, except: [:new, :create, :home]
+  before_action :set_timezone
 
   def logged_in?
     !!current_user
   end
 
   private
+
+  def set_timezone
+    Time.zone = 'GMT'
+  end
 
   def require_login
     redirect_to root_path unless logged_in?
