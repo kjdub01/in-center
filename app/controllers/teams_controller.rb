b4a967a925c27690acc4519c82d8da089ba429f6
kjdub01 <kjewettwilliams@gmail.com>
2019-04-08 21:37:19 -0700

    remove unnecessary code

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,16 +2,7 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:game_id]
-      @game = Game.find_by(id: params[:game_id])
-      if @game.nil
-        redirect_to games_path
-      else
-        @teams = @games.teams
-      end
-    else
-      @teams = Team.all
-    end
+    @teams = Team.all
   end
 
   def show
