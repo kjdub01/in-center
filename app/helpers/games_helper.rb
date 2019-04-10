5f6a51a545df97259594e0f8cbf81fdfbc2b045f
kjdub01 <kjewettwilliams@gmail.com>
2019-04-09 22:48:53 -0700

    add end_game helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -7,4 +7,9 @@ module GamesHelper
   def game_date(game)
     game.starts_at.strftime("%-m/%-d/%Y")
   end
+
+  def game_end(game)
+    end_time = game.starts_at + 80.minutes
+    end_time.strftime("%l:%M %P")
+  end
 end
