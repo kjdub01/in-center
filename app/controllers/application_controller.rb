56cbe7fad1076a36740c9e4c6b3556688ee8ae99
kjdub01 <kjewettwilliams@gmail.com>
2019-04-10 09:03:32 -0700

    add admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def admin
+    current_user.admin
+  end
+  helper_method :admin
 end
