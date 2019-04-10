c67c5abecfa0a92bf15fffd7247f05b649db15d7
kjdub01 <kjewettwilliams@gmail.com>
2019-04-10 08:36:38 -0700

    add validations back in

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,9 @@ class Game < ApplicationRecord
   accepts_nested_attributes_for :games_team
   has_many :teams, through: :games_team
 
+  validates :user_id, presence: true
+  validates :starts_at, presence: true
+
   def home_team
     which_team = self.games_team.where(home_team: true)
     home_team = Team.find(which_team[0][:team_id])
--- app/models/games_team.rb
+++ app/models/games_team.rb
@@ -1,4 +1,7 @@
 class GamesTeam < ApplicationRecord
   belongs_to :team
   belongs_to :game
+
+  validates :team_id, presence: true
+  validates :game_id, presence: true
 end
