e08c7920175182fab0db7a5066462b27decc03aa
kjdub01 <kjewettwilliams@gmail.com>
2019-04-13 19:16:55 -0700

    add class scope method unassigned

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,8 +4,9 @@ class Game < ApplicationRecord
   accepts_nested_attributes_for :games_team
   has_many :teams, through: :games_team
 
-
   validates :starts_at, presence: true
+
+  scope :unassigned, -> {where(user_id: nil)}
 
   def home_team
     which_team = self.games_team.where(home_team: true)
