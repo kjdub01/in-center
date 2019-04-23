fc42fd6222632b5ad5ce516bbb6294aa3d82497c
kjdub01 <kjewettwilliams@gmail.com>
2019-04-22 22:54:36 -0700

    add format to validation to phone and zipcode when present

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 70}
   VAILD_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255}, format: {with: VAILD_EMAIL_FORMAT, message: "format: example@address.com"}, uniqueness: { case_sensitive: false }
-
+  VALID_PHONE_FORMAT = /\A[2-9]\d{2}-\d{3}-\d{4}\z/
+  validates :phone, allow_blank: true, format: {with: VALID_PHONE_FORMAT, message: "Format XXX-XXX-XXXX"}
+  VALID_ZIPCODE_FORMAT = /\A\d{5}(-\d{4})?\z/
+  validates :zipcode, allow_blank: true, format: {with: VALID_ZIPCODE_FORMAT, message: "Format XXXXX"}
   def self.find_or_create_by_omniauth(auth_hash)
     where(email: auth_hash["info"]["email"]).first_or_create do |u|
       u.password = SecureRandom.hex
