daabfd95385aa3ae3132de61ef9e74157253b191
kjdub01 <kjewettwilliams@gmail.com>
2019-06-15 16:54:27 -0700

    finish prototype and keep rest url

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -5,12 +5,15 @@ $(() => {
 const bindClickHandlers = () => {
   $('.unassigned').on('click', e => {
     e.preventDefault()
+    history.pushState(null, null, "games/unassigned")
     fetch('/games/unassigned.json')
       .then(res => res.json())
       .then(data => {
         $('#maincontent').html('')
         data.forEach(game => {
-          console.log(game)
+          let newGame = new Game(game)
+          let gameHTML = newGame.formatIndex()
+          $('#maincontent').append(gameHTML)
         })
       })
   })
@@ -21,4 +24,11 @@ function Game(game) {
   this.starts_at = game.starts_at
   this.teams = game.teams
   this.user = game.user
-} 
+}
+
+Game.prototype.formatIndex = function() {
+  let gameHTML = `
+    <h1>${this.user}</h1>
+  `
+  return gameHTML
+}
