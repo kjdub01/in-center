ce715574aa38d4a8bf2b3c31a3ecb599c24253d3
kjdub01 <kjewettwilliams@gmail.com>
2019-06-19 19:35:53 -0700

    fix bindclickhandlers, assign teams, edit formatindex and formatShow

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,10 +3,10 @@ $(() => {
 })
 
 const bindClickHandlers = () => {
-  $('.unassigned').on('click', e => {
+  $(document).on('click', "#unassigned", function(e) {
     e.preventDefault()
-    history.pushState(null, null, "games/unassigned")
-    getGames ()
+  history.pushState(null, null, "games/unassigned")
+  getGames()
   })
 
   $(document).on('click', ".show_link", function(e) {
@@ -29,9 +29,13 @@ const bindClickHandlers = () => {
 
     $.post("/games", values).done(function(data) {
       $('#maincontent').html('')
-      const newGame = new Game(data)
-      const htmlToAdd = newGame.formatShow()
-      $('#maincontent').html(htmlToAdd)
+      let id = ($(this).attr('data-id'))
+      fetch(`/games/${id}.json`)
+        .then(res => res.json())
+        .then(game => {
+          const newGame = new Game(data)
+          const htmlToAdd = newGame.formatShow()
+          $('#maincontent').html(htmlToAdd)
     })
   })
 }
@@ -52,20 +56,26 @@ const getGames = () => {
 function Game(game) {
   this.id = game.id
   this.starts_at = game.starts_at
-  this.teams = game.teams
+  this.team1 = game.teams[0].team_name
+  this.team2 = game.teams[1].team_name
+  this.venue = game.teams[0].address1
   this.user = game.user
 }
 
 Game.prototype.formatIndex = function() {
+
   let gameHTML = `
-    <a href="/games/${this.id}" data-id="${this.id}" class="show_link"><h1>${this.starts_at}</h1>
+  <h3>Match</h3>
+    <h4><a href="/games/${this.id}" data-id="${this.id}" class="show_link"><h1>${this.starts_at}</a></h4>
+    <h4>${this.team1} vs. ${this.team2}</h4>
+    <h4>Venue: ${this.venue}</h4>
   `
   return gameHTML
 }
 
 Game.prototype.formatShow = function() {
   let gameHTML = `
-    <h1>${this.id}</h1>
+    <h1>${this.team1} vs ${this.team2}</h1>
   `
   return gameHTML
 }
